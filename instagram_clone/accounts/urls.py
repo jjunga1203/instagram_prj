8b4ad2af7e6317e77f9982df87c83ce06bb10d8d
HEEJUNG CHOI <jjunga1203@gmail.com>
2024-04-19 17:21:57 +0900

    accounts

--- instagram_clone/accounts/urls.py
+++ instagram_clone/accounts/urls.py
@@ -4,6 +4,8 @@ from . import views
 app_name = 'accounts'
 
 urlpatterns = [
-    path('accounts/', views.index, name='index'),
-    path('', views.index, name='index'),
+    path('accounts/', views.profile, name='profile'),
+    path('', views.profile, name='profile'),
+    path('signup/', views.signup, name='signup'),
+    path('login/', views.login, name='login'),
 ]
