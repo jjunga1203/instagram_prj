dec544b0461d63c64b5bc6a268efcc4be415096c
yeeunlim <yeeun.lim@ewhain.net>
2024-04-23 15:27:06 +0900

    사진 업로드

--- instagram_clone/stories/urls.py
+++ instagram_clone/stories/urls.py
@@ -2,8 +2,8 @@ from django.contrib import admin
 from django.urls import path
 from . import views
 app_name = 'stories'
+
 urlpatterns = [
     path('create/', views.create, name='create'),
-    path('<str:image_name>/', views.get_story_image, name='get_story_image'),
-    path('<int:story_id>/', views.detail, name='detail'),
+    path('detail/<int:pk>/', views.detail, name='detail'),
 ]
