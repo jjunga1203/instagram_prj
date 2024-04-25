21e5de5e9f22487e4297e6136c4d9d99e00168da
HEEJUNG CHOI <jjunga1203@gmail.com>
2024-04-25 12:07:55 +0900

    jjunga 24-04-25

--- instagram_clone/stories/urls.py
+++ instagram_clone/stories/urls.py
@@ -6,7 +6,6 @@ app_name = 'stories'
 urlpatterns = [
     path('create/', views.create, name='create'),
     path('detail/<int:pk>/', views.detail, name='detail'),
-    path('edit/<int:pk>', views.edit, name='edit'),
     path('delete/<int:pk>/', views.delete, name='delete'),
     path('archive/', views.archive, name='archive'),
 ]
